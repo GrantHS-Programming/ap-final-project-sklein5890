7ca09b923f4db577f66504ba6e5a28e316e29b87
sklein5890 <ewrety@fsrgdfh>
2024-05-23 11:57:17 -0700

    hrsthdfz

--- FinalProject.java
+++ FinalProject.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,46 +19,83 @@ public class FinalProject {
         int choice = keyboard.nextInt();
 
         if (choice == 1){
-            rolledPlayer();
+            Player player1 = new Player(rolledPlayer());
+        }
+
+        else if(choice == 2){
+            Player player1 = new Player(standardArrayPlayer());
         }
 
     }
 
-    public static void rolledPlayer(){
+    public static int[] rolledPlayer(){
         Scanner keyboard = new Scanner(System.in);
 
         int numsToGive[] = new int[6];
         String stats[] = {"strength", "dexterity",  "constitution",  "intelligence",  "wisdom",  "charisma"};
-        int rolls[] = new int[6];
+        ArrayList<Integer> rolls = new ArrayList<>();
 
         for (int x = 0; x < 6; x++){
-            rolls[x] = (int)((Math.random() * 20) + 1);
+            rolls.add((int)((Math.random() * 20) + 1));
         }
-        System.out.println("You rolled " + Arrays.toString(rolls));
-
+        System.out.println("You rolled " + rolls);
+        int l = 0;
         for (int x = 0; x < 6; x++){
-            System.out.println("What number do you want to assign to "+ stats[x] +" ?");
+            System.out.println("Which of these numbers do you want to assign to "+ stats[x] +" ?");
             keyboard.useDelimiter("\\n");
-            System.out.println(Arrays.toString(rolls));
-            int choice = keyboard.nextInt();
+            System.out.println(rolls);
+            Integer choice = keyboard.nextInt();
 
             int p = 0;
-            int m = 0;
+
             for (int z : rolls){
-                if (rolls[p] == choice){
-                    numsToGive[m] = choice;
-                }
-                else {
-                    System.out.println("not in rolls list");
+                if (z == choice){
+                    numsToGive[l] = z;
                 }
                 p++;
-                m++;
             }
-
-
-            //doenst work above
+            rolls.remove(rolls.indexOf(choice));
+            l++;
         }
+        System.out.println("Here are the final values for your player: (in order of:\"strength\", \"dexterity\",  \"constitution\",  \"intelligence\",  \"wisdom\",  \"charisma\") "+Arrays.toString(numsToGive));
+        return numsToGive;
+    }
 
+    public static int[] standardArrayPlayer(){
+        Scanner keyboard = new Scanner(System.in);
+
+        int numsToGive[] = new int[6];
+        String stats[] = {"strength", "dexterity",  "constitution",  "intelligence",  "wisdom",  "charisma"};
+        ArrayList<Integer> rolls = new ArrayList<>();
+
+        rolls.add(15);
+        rolls.add(14);
+        rolls.add(13);
+        rolls.add(12);
+        rolls.add(10);
+        rolls.add(8);
+
+        System.out.println("This is the standard array: " + rolls);
+        int l = 0;
+        for (int x = 0; x < 6; x++){
+            System.out.println("Which of these numbers do you want to assign to "+ stats[x] +" ?");
+            keyboard.useDelimiter("\\n");
+            System.out.println(rolls);
+            Integer choice = keyboard.nextInt();
+
+            int p = 0;
+
+            for (int z : rolls){
+                if (z == choice){
+                    numsToGive[l] = z;
+                }
+                p++;
+            }
+            rolls.remove(rolls.indexOf(choice));
+            l++;
+        }
+        System.out.println("Here are the final values for your player: (in order of:\"strength\", \"dexterity\",  \"constitution\",  \"intelligence\",  \"wisdom\",  \"charisma\") "+Arrays.toString(numsToGive));
+        return numsToGive;
     }
 
 
